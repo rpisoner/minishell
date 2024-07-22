ddeb5bbe24f9de7edb0f68b9feee2b795e96f5ee
jolivare <jolivare@student.42madrid.com>
2024-07-23 00:50:00 +0200

    structs and one command function

--- src/exec.c
+++ src/exec.c
@@ -1,14 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   history.c                                          :+:      :+:    :+:   */
+/*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jolivare <jolivare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/17 15:00:46 by jolivare          #+#    #+#             */
-/*   Updated: 2024/07/22 16:45:54 by jolivare         ###   ########.fr       */
+/*   Created: 2024/07/23 00:41:22 by jolivare          #+#    #+#             */
+/*   Updated: 2024/07/23 00:49:24 by jolivare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
+/**Si hay más de un comando hacemos como en el pipex, si no hardcodeada histórica*/
+
+void	one_cmd(t_exec *exec)
+{
+	pid_t	pid;
+	int		status;
+
+	pid = fork();
+	if (pid < 0)
+		// mensaje de error
+	if (pid == 0)
+		// función que ejecute el único comando
+	waitpid(pid, &status, 0);
+	exec->status = WEXITSTATUS(status);	
+}
